22df493eb9cb018146a947c150a7c49e55c53349
Loic Verrall <loic.verrall@gmail.com>
2018-02-22 18:21:09 +0000

    Android finally working again... 🎉

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to UniNinja!
+          Welcome to UniNinja!!
         </Text>
       </View>
     );
