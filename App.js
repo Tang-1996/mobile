c3f8d2788ffd636a9f8d5804ea5c1a5f25477da0
Loic Verrall <loic.verrall@gmail.com>
2018-02-22 23:03:44 +0000

    Basic demo app with single button added

--- App.js
+++ App.js
@@ -1,52 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+	Button,
+	Image,
+	Platform,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to UniNinja!!
-        </Text>
-      </View>
-    );
-  }
+	showUniversityPressed(event) {
+		console.log("Show university");
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Image source={require('./static/images/logo.png')} style={styles.logo} />
+
+				<Text style={styles.welcome}>
+					Welcome to UniNinja
+				</Text>
+
+				<Text style={styles.instructions}>
+					Press the button below to display the name of the first uni!
+				</Text>
+
+				<Button onPress={this.showUniversityPressed} title="Show University" color="#F5FCFF" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		backgroundColor: '#1a64db',
+	},
+	logo: {
+		marginTop: 60,
+		width: 60,
+		height: 60,
+	},
+	welcome: {
+		fontSize: 30,
+		textAlign: 'center',
+		color: '#F5FCFF',
+		margin: 30,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#BBBBBB',
+		marginBottom: 10,
+	},
 });
