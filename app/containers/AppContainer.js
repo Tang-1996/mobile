89be52df0f629c42191f1f1007e53e58fac3d00f
Loic Verrall <loic.verrall@gmail.com>
2018-03-19 12:39:28 +0000

    Apollo Client hooked up to application. Sample GraphQL method added to API library.

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -5,11 +5,28 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ActionCreators } from '../actions';
 
+// ApolloClient imports.
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import Api from '../lib/graphql-api';
+
 import MainTabBar from './MainTabBar';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: Api.host }),
+  cache: new InMemoryCache()
+});
 
 class AppContainer extends Component {
     render() {
-        return <MainTabBar {...this.props} />;
+        return (
+            <ApolloProvider client={client}>
+                <MainTabBar {...this.props} />
+            </ApolloProvider>
+        );
     }
 }
 
--- app/lib/graphql-api.js
+++ app/lib/graphql-api.js
@@ -1,36 +1,52 @@
-// TODO: Use ApolloJS (https://www.apollographql.com/docs/react/) to generate GraphQL requests.
+import gql from 'graphql-tag';
 
 class Api {
-  static headers() {
-    return {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'dataType': 'json',
+    static host = 'https://localhost:3000';//'https://api.uni.ninja';
+
+    static headers() {
+        return {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'dataType': 'json',
+        }
     }
-  }
 
-  static get(route) {
-    return this.xhr(route, null, 'GET');
-  }
+    static get(route) {
+        return this.xhr(route, null, 'GET');
+    }
 
-  static post(route, params) {
-    return this.xhr(route, params, 'POST');
-  }
+    static post(route, params) {
+        return this.xhr(route, params, 'POST');
+    }
 
-  static xhr(route, params, verb) {
-    const host = 'https://api.uni.ninja'
-    const url = `${host}${route}`;
+    static xhr(route, params, verb) {
+        const url = `${Api.host}${route}`;
 
-    let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null );
-    options.headers = Api.headers()
+        let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null );
+        options.headers = Api.headers()
 
-    return fetch(url, options).then( response => {
-      let json = response.json();
-      if (response.ok) {
-        return json
-      }
-      return json.then(err => {throw err});
-    }).then( json => json );
-  }
+        return fetch(url, options).then( response => {
+            let json = response.json();
+
+            if (response.ok) {
+                return json
+            }
+
+            return json.then(err => { throw err });
+        }).then( json => json );
+    }
+
+    // GraphQL Queries
+
+    static allUnis() {
+        return gql`
+            query {
+                universities {
+                    name
+                }
+            }
+        `;
+    }
 }
-export default Api
+
+export default Api;
