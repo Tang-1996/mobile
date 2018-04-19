0b02a2c7136000778c3d5250c1a9d1c18483b4cf
Loic Verrall <loic.verrall@gmail.com>
2018-04-20 00:22:55 +0100

    Tab bar overhauled and respects safe areas. Icons are now sourced from FontAwesome as vectors. Light status bar content added. Background behind status bar and dock added.

--- app/components/MainTabBar.js
+++ app/components/MainTabBar.js
@@ -1,55 +1,85 @@
 import React, { Component } from 'react';
-import { Image, Text, StyleSheet } from 'react-native';
+import {Text, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
+import FontAwesome, { Icons } from "react-native-fontawesome";
 
+// Redux imports
+import { selectTab } from "../actions/actions";
 import { connect } from 'react-redux';
 
+// Component imports
 import Search from './Search';
-import { selectTab } from "../actions/actions";
+import Welcome from './Welcome';
 
 class MainTabBar extends Component {
     render() {
-        const tabBarIconsPath = "../../static/images/tab_icons/";
-
         const { selectTab } = this.props;
 
 		return (
-            <TabNavigator>
-                <TabNavigator.Item
-                    title="Universities"
-                    selected={this.props.selectedTab == 0}
-                    renderIcon={ () => <Image source={require(tabBarIconsPath + "universities.png")} style={styles.tabBarIconImage} /> }
-                    renderSelectedIcon={ () => <Image source={require(tabBarIconsPath + "universities_selected.png")} style={styles.tabBarIconImage} /> }
-                    onPress={ () => selectTab(0) } >
-                    <Search {...this.props} />
-                </TabNavigator.Item>
+		    <SafeAreaView style={styles.safeArea}>
+                <StatusBar barStyle="light-content" />
 
-                <TabNavigator.Item
-                    title="My List"
-                    selected={this.props.selectedTab == 1}
-                    renderIcon={ () => <Image source={require(tabBarIconsPath + "mylist.png")} style={styles.tabBarIconImage} /> }
-                    renderSelectedIcon={ () => <Image source={require(tabBarIconsPath + "mylist_selected.png")} style={styles.tabBarIconImage} /> }
-                    onPress={ () => selectTab(1) } >
-                    <Text>My List</Text>
-                </TabNavigator.Item>
+                <TabNavigator tabBarStyle={styles.tabBar} >
+                    <TabNavigator.Item
+                        title="Universities"
+                        tabBarOptions={styles.tabBar}
+                        selected={this.props.selectedTab == 0}
+                        renderIcon={ () => <FontAwesome style={styles.tabBarIconImage}>{Icons.university}</FontAwesome> }
+                        renderSelectedIcon={ () => <FontAwesome style={styles.tabBarIconImageSelected}>{Icons.university}</FontAwesome> }
+                        titleStyle={styles.tabStyle}
+                        selectedTitleStyle={styles.tabStyleSelected}
+                        onPress={ () => selectTab(0) }>
+                        <Search {...this.props} />
+                    </TabNavigator.Item>
 
-                <TabNavigator.Item
-                    title="Settings"
-                    selected={this.props.selectedTab == 2}
-                    renderIcon={ () => <Image source={require(tabBarIconsPath + "settings.png")} style={styles.tabBarIconImage} /> }
-                    renderSelectedIcon={ () => <Image source={require(tabBarIconsPath + "settings_selected.png")} style={styles.tabBarIconImage} /> }
-                    onPress={ () => selectTab(2) } >
-                    <Text>Settings</Text>
-                </TabNavigator.Item>
-            </TabNavigator>
+                    <TabNavigator.Item
+                        title="My List"
+                        selected={this.props.selectedTab == 1}
+                        renderIcon={ () => <FontAwesome style={styles.tabBarIconImage}>{Icons.list}</FontAwesome> }
+                        renderSelectedIcon={ () => <FontAwesome style={styles.tabBarIconImageSelected}>{Icons.list}</FontAwesome> }
+                        titleStyle={styles.tabStyle}
+                        selectedTitleStyle={styles.tabStyleSelected}
+                        onPress={ () => selectTab(1) } >
+                        <Text>My List</Text>
+                    </TabNavigator.Item>
+
+                    <TabNavigator.Item
+                        title="Settings"
+                        selected={this.props.selectedTab == 2}
+                        renderIcon={ () => <FontAwesome style={styles.tabBarIconImage}>{Icons.cog}</FontAwesome> }
+                        renderSelectedIcon={ () => <FontAwesome style={styles.tabBarIconImageSelected}>{Icons.cog}</FontAwesome> }
+                        titleStyle={styles.tabStyle}
+                        selectedTitleStyle={styles.tabStyleSelected}
+                        onPress={ () => selectTab(2) } >
+                        <Welcome {...this.props} />
+                    </TabNavigator.Item>
+                </TabNavigator>
+            </SafeAreaView>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#12438f'
+    },
+    tabBar: {
+        backgroundColor: '#12438f',
+    },
     tabBarIconImage: {
-        height: 26,
-        width: 26
+        color: 'rgb(112,112,149)',
+        fontSize: 22
+    },
+    tabBarIconImageSelected: {
+        color: 'rgb(112,176,249)',
+        fontSize: 22
+    },
+    tabStyle: {
+        color: 'rgb(112,112,149)'
+    },
+    tabStyleSelected: {
+        color: 'rgb(112,176,249)'
     }
 });
 
