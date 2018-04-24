6ca4e0df8d75326b3cb96391e04351c4deecf3b7
Alex Tang <alextang@localhost.susx.ac.uk>
2018-04-24 14:50:57 +0100

    testin

---
+++ __tests__/App.js
@@ -0,0 +1,33 @@
+import * as types from '../app/actions/types'
+
+console.log(types.FAVOURITE_UNI)
+
+test('Demo', () => {
+  expect(0).toEqual(0)
+})
+
+
+// import * as actions from '../app/actions/actions';
+// import * as types from '../app/actions/types';
+
+// import actions from '../app/actions/actions';
+// import 'react-native';
+// import * as types from '../app/actions/types';
+//
+// console.log(types.FAVOURITE_UNI)
+//
+// test('Demo test blah blah', () => {
+//   expect(0).toEqual(0)
+// })
+
+
+// describe('actions', () => {
+//   it('should create an action to add a todo', () => {
+//     const text = 'Finish docs'
+//     const expectedAction = {
+//       type: types.ADD_TODO,
+//       text
+//     }
+//     expect(0).toEqual(0)
+//   })
+// })
