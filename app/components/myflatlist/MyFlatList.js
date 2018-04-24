ff2f2e0c37f6f59fe954dd16f3a04fb10cd32e4d
Loic Verrall <loic.verrall@gmail.com>
2018-04-24 23:02:24 +0100

    MyFlatList and ListItem implemented as general uni-displaying lists

---
+++ app/components/myflatlist/MyFlatList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import {FlatList} from 'react-native'
+
+import ListItem from './ListItem'
+
+export default class MyFlatList extends Component {
+  renderItem (item) {
+    return (
+      <ListItem
+        uni={item}
+        onPressItem={this.props.onPressItem} />
+    )
+  }
+
+  render () {
+    const { data, style } = this.props
+
+    return (
+      <FlatList
+        style={style}
+        data={data}
+        renderItem={({item}) => this.renderItem(item)}
+        keyExtractor={(item, index) => item.name} />
+    )
+  }
+}
